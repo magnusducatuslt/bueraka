11263c4260686770adc88b1637909f7fef88978e
Mr.CodeRewier <magnusducatuslt>
2019-03-05 23:28:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const conf = new Map(bracketsConfig)
+  const string = str.split('')
+  const result = string.reduce((array, item) => {
+    if (array.length === 0) {
+      return conf.has(item) ? [...array, item] : [...array, item]
+    } else {
+      if (conf.get(array[array.length - 1]) === item) {
+        array.pop()
+        return [...array]
+      } else {
+        return [...array, item]
+      }
+    }
+  }, [])
+  return result.length === 0 ? true : false
 }
